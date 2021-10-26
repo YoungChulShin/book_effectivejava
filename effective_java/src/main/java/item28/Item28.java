0a39f3b59bcc36b4e6a5c321f35d957689e0caa2
YoungChulShin <go1323@gmail.com>
2021-10-27 06:47:10 +0900

    item 28 테스트 추가

--- effective_java/src/main/java/item28/Item28.java
+++ effective_java/src/main/java/item28/Item28.java
@@ -8,5 +8,9 @@ public class Item28 {
   public void playground() {
     // 컴파일 오류
     // List<Object> objectList = new ArrayList<Long>();
+
+    Object[] objectArray = new Long[1];
+    objectArray[0] = "hihi";
+
   }
 }
---
+++ effective_java/src/main/java/item28/Main.java
@@ -0,0 +1,9 @@
+package item28;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Item28 item28 = new Item28();
+    item28.playground();
+  }
+}
