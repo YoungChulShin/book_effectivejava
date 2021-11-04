8c204f5629a24887a16e39e18d53043ece0baff1
YoungChulShin <go1323@gmail.com>
2021-11-04 23:49:31 +0900

    item34

---
+++ effective_java/src/main/java/item34/Main.java
@@ -0,0 +1,14 @@
+package item34;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Planet[] values = Planet.values();
+    for (Planet planet :values) {
+
+      System.out.println(planet);
+      System.out.println(planet.name());
+    }
+  }
+
+}
---
+++ effective_java/src/main/java/item34/Operation2.java
@@ -0,0 +1,33 @@
+package item34;
+
+public enum Operation2 {
+
+  PLUS("+") {
+    public double apply(double x, double y) {
+      return x + y;
+    }
+  },
+  MINUS("-") {
+    public double apply(double x, double y) {
+      return x - y;
+    }
+  },
+  TIMES("*") {
+    public double apply(double x, double y) {
+      return x * y;
+    }
+  },
+  DIVIDE("/") {
+    public double apply(double x, double y) {
+      return x / y;
+    }
+  };
+
+  private final String symbol;
+
+  Operation2(String symbol) {
+    this.symbol = symbol;
+  }
+
+  public abstract double apply(double x, double y);
+}
