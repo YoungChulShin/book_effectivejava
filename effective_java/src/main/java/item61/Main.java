5292f011df99218a64797b022ec5bfabea5224cc
YoungChulShin <go1323@gmail.com>
2021-11-26 06:54:44 +0900

    item 61

---
+++ effective_java/src/main/java/item61/Main.java
@@ -0,0 +1,17 @@
+package item61;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Integer a = 4;
+    Integer b = 4;
+
+    if (a == b) {
+      System.out.println("same");
+    }
+
+    System.out.println("not same");
+
+  }
+
+}
